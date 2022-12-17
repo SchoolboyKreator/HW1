87d0246f7360a60e76b2e5446c08c8b9bd6bf597
SchoolboyKreator <dmitryshtef@gmail.com>
2022-12-17 13:44:54 +0300

    Hometask #1

---
+++ hws2-master/src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessagePropsType} from "../message/Message";
+
+// создать тип вместо any и отобразить приходящие данные
+const FriendMessage = (props: MessagePropsType) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <div id={'wrapper-avatar-time'} className={'wrapper-avatar-time'}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.id}
+                    // создаёт студент
+                    src={props.message.user.avatar}
+                    alt={'Ivan'}
+                    //
+                />
+                    <div
+                        id={'hw1-friend-time-' + props.message.id}
+                        className={s.friendTime}
+                    >
+                        {/*создаёт студент*/}
+                        {props.message.message.time}
+                        {/**/}
+                    </div>
+                </div>
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.id}
+                        className={s.friendName}
+                    >
+                        {/*создаёт студент*/}
+                        <p>{props.message.user.name}</p>
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.id}
+                        className={s.friendMessageText}
+                    >
+                        {/*создаёт студент*/}
+                        {props.message.message.text}
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default FriendMessage
